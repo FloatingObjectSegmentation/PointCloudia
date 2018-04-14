3d567edaeb1db783387200c4124085d8e45f5971
mirceta <kristijan.mirceta@gmail.com>
2018-04-14 13:05:06 +0200

    A point cloud gets loaded successfully

--- Source/PointCloudSelecting/PointCloudRenderingComponent.cpp
+++ Source/PointCloudSelecting/PointCloudRenderingComponent.cpp
@@ -20,9 +20,16 @@ void UPointCloudRenderingComponent::BeginPlay()
 	Super::BeginPlay();
 
 	TArray<FPointCloudPoint> LoadedPoints;
-	EPointCloudColorMode Mode = EPointCloudColorMode::None;
-	FPointCloudFileHeader Header = FPointCloudHelper::ReadFileHeader(TEXT("C:\\Users\\km\\Desktop\\graphics\\data.txt"));
-	FPointCloudHelper::ImportAsText(TEXT("C:\\Users\\km\\Desktop\\graphics\\data.txt"), LoadedPoints, Mode, 0, 10, Header);
+	EPointCloudColorMode Mode = EPointCloudColorMode::RGB;
+	FPointCloudFileHeader Header = FPointCloudHelper::ReadFileHeader(TEXT("C:\\Users\\km\\Desktop\\graphics\\data\\slama.txt"));
+
+	TArray<int32> SelectedColumns;
+	for (int32 i = 0; i < 6; i++) {
+		SelectedColumns.Add(i);
+	}
+	Header.SelectedColumns = SelectedColumns;
+
+	FPointCloudHelper::ImportAsText(TEXT("C:\\Users\\km\\Desktop\\graphics\\data\\slama.txt"), LoadedPoints, Mode, 0, 10, Header);
 	int32 NumberOfPoints = LoadedPoints.Num();
 	UE_LOG(LogTemp, Warning, TEXT("PointCloudLoaded %d"), NumberOfPoints);
 }
